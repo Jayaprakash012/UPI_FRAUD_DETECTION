d1f64664780c88861cc6b3b4a64726c0b3c6f3ff
Jaya Prakash Sahoo <Jayaprakashsahoo012@gmail.com>
2026-04-14 13:25:14 +0530

    Fix variable name from 'routes' to 'route' in for loop

--- test_routes.py
+++ test_routes.py
@@ -3,7 +3,7 @@ from app import app
 route = ['/', '/analysis', '/qr_upload', '/transaction', '/get_chart_data', '/nonexistent']
 
 with app.test_client() as c:
-    for r in routes:
+    for r in route:
         resp = c.get(r)
         print(r, resp.status_code)
         data = resp.get_data(as_text=True)
